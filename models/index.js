24ea019270af606ed7557655516d5d022ab35eca
Deluxe Knives <contact@deluxeknives.com>
2022-10-16 18:19:45 -0500

    Nonce creation endpoint

---
+++ models/index.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const nonceSchema = mongoose.Schema({
+    nonce: {
+        type: String,
+        required: true
+    },
+    nftId: {
+        type: Number,
+        required: true
+    }
+});
+
+const redemptionSchema = mongoose.Schema({
+    redeemed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    nftId: {
+        type: Number,
+        required: true
+    },
+    redeemedDate: {
+        type: Date,
+        required: false
+    }
+});
+
+const Nonce = mongoose.model('Nonce', nonceSchema);
+const Redemption = mongoose.model('Redemption', redemptionSchema);
+
+export { Nonce, Redemption };
--- routes/redemption.js
+++ routes/redemption.js
@@ -1,21 +1,58 @@
 import { Router } from "express";
+import { Nonce, Redemption } from '../models/index.js';
+import { v4 } from 'uuid';
 
 const router = Router();
 
 // Returns true if it has been redeemed, false if it has not
-router.get('/check/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if(isNaN(id) || id < 0) res.status(400).json("Incorrect ID!");
+router.get('/check/:nftID', async (req, res) => {
+    const nftID = parseInt(req.params.nftID);
+    if(isNaN(nftID) || nftID < 0) res.status(400).json("Incorrect ID!");
     else {
-        // Attempt get from database
-        res.status(200).json(true);
+        try {
+            const redemption = await Redemption.find({ $where: `this.nftID==${nftID}` });
+            res.status(200).json(redemption.get("id"));
+        }
+        catch (err) {
+            res.status(200).json(false);
+        }
     }
 });
 
-// Get Nonce
+// Creates a Nonce, doesn't overwrite a previous one
+router.post('/getNonce/:nftID', async (req, res) => {
+    // Ensures that nftID is right
+    const nftID = parseInt(req.params.nftID);
+    if(isNaN(nftID) || nftID < 0) res.status(400).json("Incorrect ID!");
 
+    // Generate nonce
+    const nonce = v4();
+    const nonceObj = new Nonce({ nonce, nftId: nftID });
+
+    // Writes to database
+    try {
+        const newNonce = await nonceObj.save();
+        res.status(201).json({ nonce, id: newNonce.id });
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Redeem
+router.post('/redeemMirror', (req, res) => {
+
+    // Check that nonce is right, & delete all nonces of user
+
+    // Check for redemption
+
+    // Generate code via shopify
+
+    // Set in db what the code was & that it was redeemed
+
+    // Return
+
+});
 
 
 export default router;
