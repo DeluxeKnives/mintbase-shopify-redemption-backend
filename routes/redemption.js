073234911ff9b5f3fa488270bc118084204cd2ff
Deluxe Knives <contact@deluxeknives.com>
2022-10-16 23:26:02 -0500

    created shopify requeests

--- routes/redemption.js
+++ routes/redemption.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { Nonce, Redemption } from '../models/index.js';
 import { v4 } from 'uuid';
-import Shopify from "@shopify/shopify-api";
-import { DiscountCode } from '@shopify/shopify-api/dist/rest-resources/2022-10/index.js';
+import axios from 'axios';
 
 const router = Router();
 
@@ -41,33 +40,6 @@ router.get('/getNonce/:nftID', async (req, res) => {
     }
 });
 
-// Shopify login
-router.get('/shopify/login', async (req, res) => {
-    let authRoute = await Shopify.Auth.beginAuth(
-        req,
-        res,
-        process.env.SHOPIFY_DOMAIN,
-        '/redemption/shopify/callback',
-        false,
-    );
-    return res.redirect(authRoute);
-});
-router.get('/shopify/callback', async (req, res) => {
-    try {
-        const session = await Shopify.Auth.validateAuthCallback(
-            req,
-            res,
-            req.query //as unknown as AuthQuery
-        ); // req.query must be cast to unknown and then AuthQuery in order to be accepted
-
-        activeShopifyShop = session;
-        console.log(session.accessToken);
-    } catch (error) {
-        console.error(error); // in practice these should be handled more gracefully
-    }
-});
-let activeShopifyShop;
-
 // Redeem
 router.post('/redeemMirror', async (req, res) => {
     // Get data
@@ -101,20 +73,59 @@ router.post('/redeemMirror', async (req, res) => {
     // TODO: delete user nonces
 
     // Generate code via shopify
-    //const test_session = await Utils.loadCurrentSession(request, response); //TODO: proper setup
-    const test_session = activeShopifyShop;
-    console.log(test_session);
-    const discount_code = new DiscountCode({ session: test_session });
-    discount_code.price_rule_id = 507328175;
-    discount_code.code = "NFT-" + v4().slice(0, 15);
-    await discount_code.save({
-        update: true,
-    });
+    const priceRuleId = 507328175; // TODO: generate correct price rule from mintbase info
+    const redemptionCode = "NFT-" + v4().slice(0, 15)
+    try {
+        // Creates a price rule for this specific user
+        let shopifyRes = await axios.post(
+            `https://${process.env.SHOPIFY_DOMAIN}/admin/api/2022-10/price_rules.json`, 
+            {
+                'title': redemptionCode,
+                "value_type":"percentage",
+                "value":"-100.0",
+                "customer_selection":"all",
+                "target_type":"line_item",
+                "target_selection":"entitled",
+                "allocation_method":"each",
+                "starts_at":"2018-03-22T00:00:00-00:00",
+                "entitled_product_ids":[4670662017099], // TODO: get from mintbase info
+                "allocation_limit": 1,
+                "once_per_customer": true,
+                "usage_limit": 1
+            },
+            {
+                headers: {
+                    'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_CODE,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        console.log(shopifyRes);
+
+        // Creates a discount code
+        shopifyRes = await axios.post(
+            `https://${process.env.SHOPIFY_DOMAIN}/admin/api/2022-10/price_rules/${priceRuleId}/discount_codes.json`, 
+            {
+                'discount_codes': [{ 'code': redemptionCode }]
+            },
+            {
+                headers: {
+                    'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_CODE,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        console.log(shopifyRes);
+    }
+    catch (err) {
+        res.status(500).json(err);
+        return;
+    }
 
     // Set in db what the code was & that it was redeemed
     try {
         const redemption = new Redemption({
-            redemptionCode: discount_code.code,
+            redemptionCode,
             nftId: nftID,
             redeemed: true,
             redeemedDate: Date.now()
